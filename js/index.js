e42dc48ff1b58794b2ff79eb2f06cb3f5de219ec
Thomas <thomastex.2013@gmail.com>
2022-09-14 00:39:54 -0300

    correcion de entrega

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ let option;
 let money;
 let reason;
 let gasto;
+let resto;
 
 const wallet = [
     {
@@ -13,9 +14,13 @@ const wallet = [
         reason: "plata en calle",
     },
 ];
+function sumaWallet() {
+    let sumarMoney = wallet.reduce((acc, el) => acc + el.money, 0);
+    return (sumarMoney);
+}
 
 while(option !==0){
-    option = Number(prompt("Ingrese una de las siguientes opciones:!\n1. Registrar ingreso\n2. Registrar gasto\n3. Consultar saldo\n0. Salir"))
+    option = Number(prompt("Ingrese una de las siguientes opciones:!\n1. Registrar ingreso\n2. Registrar gasto\n3. Consultar saldo\n4. Consultar transaccion\n0. Salir"))
     
     switch (option) {
         case 1:
@@ -26,10 +31,18 @@ while(option !==0){
         case 2:
                 gasto = Number(prompt("Ingrese el monto en números y sin puntos. "));
                 reason = prompt("Motivo del gasto de dinero: ");
-                recordIncome (-gasto, reason);
+                resto = sumaWallet() -gasto;
+                if (resto < 0) {
+                    alert("Gastas mas dinero de lo que tenes");
+                } else{
+                    recordIncome (-gasto, reason);
+                }
             break;
         case 3:
-                sumaWallet();
+                resto = sumaWallet();
+            break;
+        case 4:
+                filtro();
             break;
         case 0:
                 alert("Gracias por pasar, salu2");
@@ -44,7 +57,15 @@ function recordIncome(num, ras) {
     wallet.push({money: num, reason: ras});
 }
 
-function sumaWallet() {
-    let sumarMoney = wallet.reduce((acc, el) => acc + el.money, 0);
-    alert(sumarMoney);
+function filtro() {
+    while (true){
+        let transaccion = Number(prompt("La cantidad de transacciones realizadas es de " + wallet.length + " transacciones.\n Que transaccion desea ver?"));
+        if (transaccion > 0 && transaccion <= wallet.length){
+        transaccion = transaccion - 1;
+        alert("La transaccion elegida es de " + wallet[transaccion].money + ". Motivo: " + wallet[transaccion].reason);
+        break;
+        } else {
+            alert("Seleccione un valor dentro de las transacciones realizadas");
+        }
+    }
 }
